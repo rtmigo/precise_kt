25acafc549cfccd590777a57b80197c9ddd40675
Artem IG <ortemeo@gmail.com>
2022-10-12 08:13:57 +0300

    removed old CI files

--- update_deps.sh
+++ update_deps.sh
@@ -1,4 +1,4 @@
 hubget https://github.com/rtmigo/cicd/blob/dev/github/kotlin_library_wf.yaml \
   .github/workflows/
 
-hubget https://github.com/rtmigo/cicd/blob/dev/github/gradle_pkgver_not_on_gh.py .github/workflows/
+hubget https://github.com/rtmigo/cicd/blob/dev/github/gradle_pkgver_not_on_gh.py .github/
