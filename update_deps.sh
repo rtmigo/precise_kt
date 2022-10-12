97e9e47caf37c0af45a019539842906ad9aed47e
Artem IG <ortemeo@gmail.com>
2022-10-12 08:53:31 +0300

    upd

--- update_deps.sh
+++ update_deps.sh
@@ -2,3 +2,4 @@ hubget https://github.com/rtmigo/cicd/blob/dev/github/kotlin_library_wf.yaml \
   .github/workflows/
 
 hubget https://github.com/rtmigo/cicd/blob/dev/github/gradle_pkgver_not_on_gh.py .github/
+hubget https://github.com/rtmigo/cicd/blob/dev/github/on_release_gradle_publish_gh.yaml .github/workflows/
