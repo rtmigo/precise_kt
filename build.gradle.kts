01748c704ade6f49fb8507d3c39491ead4ce6d43
Artem IG <ortemeo@gmail.com>
2022-10-20 21:52:39 +0300

    ci with mavence

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,57 @@ plugins {
     id("org.jetbrains.dokka") version "1.7.20"
     id("java-library")
     java
+    id("maven-publish")
 }
+
+
+lateinit var central: Publication
+
+publishing {
+    publications {
+        central = create<MavenPublication>("cenTrAl") {
+            from(components["java"])
+            pom {
+                val repo = "precise_kt"
+                name.set("precise")
+                description.set("Kotlin/JVM compensated summation of Double sequences to calculate sum, mean, standard deviation")
+                url.set("https://github.com/rtmigo/$repo")
+                developers {
+                    developer {
+                        name.set("Artsiom iG")
+                        email.set("ortemeo@gmail.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git://github.com/rtmigo/$repo.git")
+                    url.set("https://github.com/rtmigo/$repo")
+                }
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://github.com/rtmigo/$repo/blob/HEAD/LICENSE")
+                    }
+                }
+            }
+        }
+    }
+}
+
+//signing {
+//    useInMemoryPgpKeys(signingKey, signingPassword)
+//    sign(central)
+//}
+
+
+
 
 group = "io.github.rtmigo"
 version = "0.1.0-dev24"
 
 java {
     withSourcesJar()  // для Maven Central
+    withJavadocJar()
 }
 
 repositories {
@@ -22,7 +66,7 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
-    testImplementation("io.kotest:kotest-assertions-core:5.5.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.5.1")
 }
 
 kotlin {
---
+++ test_as_maven_package.py
@@ -0,0 +1,70 @@
+import json
+import subprocess
+from pathlib import Path
+
+import time
+from rtmaven import prepare, stage, promote, Package, eprint, eprint_header
+from tempground import TempGround
+
+
+def test_package(maven_url: str, ver: str):
+    with TempGround(
+            files={
+                # minimalistic build script to use the library
+                "build.gradle.kts": """
+                    plugins {
+                        id("application")
+                        kotlin("jvm") version "1.6.20"
+                    }
+    
+                    repositories { 
+                        maven { url = uri("__TEMP_REPO__") }
+                        mavenCentral() 
+                    }
+                    
+                    application { mainClass.set("MainKt") }
+    
+                    dependencies {
+                        implementation("io.github.rtmigo:precise:__VERSION__")
+                    }
+                """.replace("__VERSION__", ver)
+                        .replace("__TEMP_REPO__", maven_url),
+
+                # kotlin code that imports and uses the library
+                "src/main/kotlin/Main.kt": """
+                    import io.github.rtmigo.precise.*
+                    
+                    fun main() {
+                        println(listOf(1.0, 2.0).preciseSumOf {it})
+                    }
+                """}
+    ) as app:
+        eprint(app.files_content())
+        result = app.run(["gradle", "run", "-q"])
+        eprint(result)
+
+        assert result.returncode == 0
+        assert result.stdout == "3.0\n", result.stdout
+
+    eprint("Everything is OK!")
+
+
+def build_test_release():
+    js = json.loads(subprocess.check_output(
+        ["java", "-jar", "/tmp/mavence.jar", "local", "io.github.rtmigo:precise"]))
+    test_package(js["mavenRepo"], js["version"])
+    # package = stage(prepare(
+    #     description="Kotlin/JVM compensated summation of Double sequences "
+    #                 "to calculate sum, mean, standard deviation",
+    #     github_url="https://github.com/rtmigo/precise_kt@master",
+    #     developer="Artsiom iG <ortemeo@gmail.com>",
+    #     license="MIT"))
+    #
+    # eprint_header("Testing")
+    # eprint()
+    # test_package(package)
+    # promote(package)
+
+
+if __name__ == "__main__":
+    build_test_release()
